015551b1eee1da8aa614b52a74e2704154b0c36d
AkshayaP <v.bagyalakshmi1978@gmail.com>
2024-07-30 11:54:12 +0530

    Add project files.

---
+++ WebFormSamples/Samples/LabelSample/LabelSample.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.UI;
+
+namespace WebFormSamples.Samples
+{
+    public partial class LabelSample : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
